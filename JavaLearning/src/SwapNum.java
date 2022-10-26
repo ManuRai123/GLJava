79b9be4677dba968d623d8e60daa18c946b84d5b
manuroy <manuroy@MANUROY-PZS21.cisco.com>
2022-10-26 23:17:14 +0530

    swap number programme

---
+++ JavaLearning/src/SwapNum.java
@@ -0,0 +1,15 @@
+
+public class SwapNum {
+
+	public static void main(String[] args) {
+		int a=10,b=15;
+		
+		System.out.println("before swap : "+a+" " +b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("after swap : "+a+" "+b);
+
+	}
+
+}
