03603827305fab8b144c1e075bbcc6228bf7f120
manuroy <manuroy@MANUROY-PZS21.cisco.com>
2022-10-26 21:16:19 +0530

    Airthmetic operation

---
+++ JavaLearning/src/AirthmeticOp.java
@@ -0,0 +1,14 @@
+
+public class AirthmeticOp {
+
+	public static void main(String[] args) {
+		float num1=12.0f,num2=2.0f;
+		
+		System.out.println("add : "+(num1+num2));
+		System.out.println("sub : "+(num1-num2));
+		System.out.println("mul : "+(num1*num2));
+		System.out.println("divison : "+(num1/num2));
+
+	}
+
+}
