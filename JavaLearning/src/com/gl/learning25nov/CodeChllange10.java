cf21e3a66d8a2587895249c35e411b11ffa3d48d
manuroy <manuroy@CSCO-W-PF462VY8.cisco.com>
2022-11-26 15:11:23 +0530

    week 20-26 Nov learning

---
+++ JavaLearning/src/com/gl/learning25nov/CodeChllange10.java
@@ -0,0 +1,34 @@
+package com.gl.learning25nov;
+
+class InheritanceA{
+	int r=10;
+	final float pi=3.14f;
+	void areaOfCirlce() {
+		System.out.println("are of circle"+(pi*r*r));
+	}
+
+}
+class InheritanceB extends InheritanceA{
+	int l=12,b=14;
+	void areaOfRectangle() {
+		System.out.println("area of rectangle"+(l*b));
+	}l̥
+
+}
+class  InheritanceC extends InheritanceB {
+	int l=10,h=15;
+	void areaOfTraingle() {
+		System.out.println("area of trangle"+(0.5*l*h));
+	}
+}
+public class CodeChllange10 {
+
+	public static void main(String[] args) {
+		InheritanceC obj=new InheritanceC();
+		obj.areaOfTraingle();
+		obj.areaOfRectangle();
+		obj.areaOfCirlce();
+
+	}
+
+}
---
+++ JavaLearning/src/com/gl/learning25nov/CodingChallange9.java
@@ -0,0 +1,12 @@
+package com.gl.learning25nov;
+
+public class CodingChallange9 {
+	void copyMe() {
+		System.out.println("Inside copy me method");
+	}
+
+	public static void main(String[] args) {
+		CodingChallange9 obj = new CodingChallange9();
+		obj.copyMe();
+	}
+}
